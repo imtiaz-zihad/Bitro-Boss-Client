eac734162a7296061ec9856288b5a9ace7baf227
Imtiaz-Zihad <mdimtiazzihad@gmail.com>
2024-12-29 23:27:43 +0600

    Add Custom Hook

--- src/pages/Home/PopularMenu.jsx
+++ src/pages/Home/PopularMenu.jsx
@@ -1,16 +1,12 @@
-import { useEffect, useState } from "react";
+
 import SectionTitle from "../../componentes/SectionTitle/SectionTitle";
 import MenuItem from "../Shared/MenuItem/MenuItem";
+import useMenu from "../../assets/hooks/useMenu";
 
 const PopularMenu = () => {
-  const [menu, setMenu] = useState([]);
-  useEffect(() => {
-    fetch("menu.json")
-      .then((res) => res.json())
-      .then((data) => {
-        const popularItems = data.filter(item => item.category === 'popular') ;
-        setMenu(popularItems)});
-  }, []);
+
+  const [menu] = useMenu();
+  const popular = menu.filter(item => item.category === 'popular') ;
   return (
     <div className="mb-12">
       <SectionTitle
@@ -18,11 +14,13 @@ const PopularMenu = () => {
         subHeading="Popular Items"
       ></SectionTitle>
       <div className="grid md:grid-cols-2 gap-10">
-        {
-            menu.map(item => <MenuItem key={item._id} item={item}></MenuItem>)
-        }
+        {popular.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
       </div>
-      <button className="btn btn-outline border-0 border-b-4 mt-4">View Full Menu</button>
+      <button className="btn btn-outline border-0 border-b-4 mt-4">
+        View Full Menu
+      </button>
     </div>
   );
 };
