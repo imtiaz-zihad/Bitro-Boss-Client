e6ed2cf457083dd894c892d140d528afb8002bef
Imtiaz-Zihad <mdimtiazzihad@gmail.com>
2024-12-29 12:59:51 +0600

    Category Swiper added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "./Banner";
+import Category from "./Category";
 
 
 const Home = () => {
     return (
         <div>
            <Banner />
+           <Category />
         </div>
     );
 };
